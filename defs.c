20c1b1d4df011628f31053942013052bfa18a77a
ape ish <dyjasyza@gmail.com>
2026-02-28 12:06:34 +0800

    Add User typedef and Player struct

--- defs.c
+++ defs.c
@@ -4,6 +4,17 @@
 #define BOARDER_SIZE_HORIZONTAL 20
 
 //--------------------------------------------------
+// Typedefs
+//--------------------------------------------------
+typedef char User[37];
+
+typedef struct{
+    User name;
+    int wins;
+    int highestScore;
+} Player;
+
+//--------------------------------------------------
 // Main Menu Text
 //--------------------------------------------------
 #define NEW_GAME "New Game"
