41387c3ca93e1bdc652ac2d2b23a2ed823c73b5a
ape ish <dyjasyza@gmail.com>
2026-03-22 17:44:29 +0800

    feat: Add getNumPlaying to prompt number of players for the game

--- game.c
+++ game.c
@@ -10,3 +10,23 @@
 #include <string.h>
 
 #include "defsDy.h"
+
+/* ----- function implementation ----- */
+
+/**
+ * Ask the user how many players will be playing
+ * 
+ * @param numGamePlayers Where the users input will be stored
+ */
+void getNumPlaying(int *numGamePlayers)
+{
+    do{
+        printf("How many players? ");
+        scanf("%d", numGamePlayers);
+        while(getchar() != '\n');
+
+        if(*numGamePlayers < MIN_GAME_PLAYERS || *numGamePlayers > MAX_GAME_PLAYERS)
+            printf("Invalid input. Minimum of %d players and maximum of %d players\n\n", MIN_GAME_PLAYERS, MAX_GAME_PLAYERS);
+
+    }while(*numGamePlayers < MIN_GAME_PLAYERS || *numGamePlayers > MAX_GAME_PLAYERS);
+}
