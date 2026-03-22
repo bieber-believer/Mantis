3e633f95af69a8738bcc06880c44e7f1b1a39cd7
ape ish <dyjasyza@gmail.com>
2026-03-22 13:55:23 +0800

    chore: Add header description to files.c

---
+++ files.c
@@ -0,0 +1,6 @@
+/******************************************************************************
+ *  Description     : Handles the functions for file processing
+ *  Author/s        : Dy, Jasmine Yzabelle
+ *  Section         : S22A
+ *  Last Modified   : March 22, 2026
+ ******************************************************************************/
